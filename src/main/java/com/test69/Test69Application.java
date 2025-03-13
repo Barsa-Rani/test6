e910856d98fb07f3983ffded372215f37050037c
Barsa-Rani <nayakbarsarani2003@gmail.com>
2025-03-13 12:02:10 +0530

    first commit

---
+++ src/main/java/com/test69/Test69Application.java
@@ -0,0 +1,13 @@
+package com.test69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test69Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test69Application.class, args);
+	}
+
+}
