e642ad7a0ecb63e1be023526fb02cf5d4f0eb66f
Barsa-Rani <nayakbarsarani2003@gmail.com>
2025-03-13 12:06:38 +0530

    Second commit

--- src/main/java/com/test69/Test69Application.java
+++ src/main/java/com/test69/Test69Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test69Application {
 
 	public static void main(String[] args) {
+int x=100;
 		SpringApplication.run(Test69Application.class, args);
 	}
 
